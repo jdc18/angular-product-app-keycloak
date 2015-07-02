b03df4c6100694a567a12fa0276691014f5ab547
jdc <jdc@localhost>
2015-07-02 17:02:36 -0500

    First commit with the basic of teh project, hopefully I cleaned all the code

---
+++ angular-product-app2/README.md
@@ -0,0 +1,81 @@
+angular-product-app2: Assortment of technologies including Arquillian
+========================
+Author: Juan Diego Calle
+Level: Intermediate
+Technologies: AngularJS JAX-RS
+Summary: Another app
+
+What is it?
+-----------
+
+This is your project! It is a sample, deployable Maven 3 project, but you really dont need maven
+
+System requirements
+-------------------
+
+You need Bower running
+A web server like Apache, Nginx, Apache Tomcat, or JBoss/WildFly for the this page. 
+A server running JBoss/Wildfly running Keycloak's Database-service to request the products
+
+Configure Keycloak
+----------------------------
+
+On the keycloak server create a realm and add Public Client (Acces Type Public), with the proper Valid Redirect URIs, and Web Origins
+Copy the proper keycloak.json file to usr/main/web/app
+
+Install Angular Dependencies
+----------------------------
+
+Go to usr/main/webapp and run bower install
+
+        bower install
+ 
+Configure Maven
+---------------
+
+If you have not yet done so, you must [Configure Maven](https://github.com/jboss-developer/jboss-developer-shared-resources/blob/master/guides/CONFIGURE_MAVEN.md) before testing the quickstarts.
+
+
+Start JBoss WildFly with the Web Profile
+-------------------------
+
+1. Open a command line and navigate to the root of the JBoss server directory.
+2. The following shows the command line to start the server with the web profile:
+
+        For Linux:   JBOSS_HOME/bin/standalone.sh
+        For Windows: JBOSS_HOME\bin\standalone.bat
+
+ 
+Build and Deploy the Quickstart
+-------------------------
+
+_NOTE: The following build command assumes you have configured your Maven user settings. If you have not, you must include Maven setting arguments on the command line. See [Build and Deploy the Quickstarts](https://github.com/jboss-developer/jboss-eap-quickstarts#build-and-deploy-the-quickstarts) for complete instructions and additional options._
+
+1. Make sure you have started the JBoss Server as described above.
+2. Open a command line and navigate to the root directory of this quickstart.
+3. Type this command to build and deploy the archive:
+
+        mvn clean package wildfly:deploy
+
+4. This will deploy `target/angular-product-app2.war` to the running instance of the server.
+
+Or just mvn install or just copy src/main/webapp to your web server.  Running your page without a web server will give you trouble when loading external files like keycloak.json or any $http request.
+ 
+
+Access the application 
+---------------------
+
+The application will be running at the following URL: <http://localhost:8080/angular-product-app2/>.
+
+
+Undeploy the Archive
+--------------------
+Only if you are running on JBoss/Wildfly
+
+1. Make sure you have started the JBoss Server as described above.
+2. Open a command line and navigate to the root directory of this quickstart.
+3. When you are finished testing, type this command to undeploy the archive:
+
+        mvn wildfly:undeploy
+
+
---
+++ angular-product-app2/src/main/webapp/js/app.js
@@ -0,0 +1,158 @@
+(function() {
+    var app = angular.module('myApp', ['ngRoute']);
+
+    app.controller('ProductController', function($scope, $http, keycloakLauncher,initService) {
+
+        $scope.loggedIn = initService.isLoggedIn();
+        $scope.user = initService.getUserInfo();
+
+        $scope.products = [];
+        $scope.reloadData = function() {
+            $http.get("https://localhost:8443/unika/usuarios").success(function(data) {
+                $scope.products = angular.fromJson(data);
+            });
+        };
+
+        $scope.logout = function(){
+            console.log('*** LOGOUT');
+            keycloakLauncher.loggedIn = false;
+            keycloakLauncher.keycloak = null;
+            window.location = keycloakLauncher.logoutUrl;
+        };
+        $scope.login = function () {
+            console.log('*** LOGIN');
+            keycloakLauncher.keycloak.login();
+        };
+
+    });
+
+    app.provider('keycloakLauncher', function keycloakLauncherProvider() {
+        this.keycloak = {};
+        this.loggedIn = false;
+        this.logoutUrl = "/";
+
+        this.$get =  function () {
+            var keycloak = this.keycloak;
+            var loggedIn = this.loggedIn;
+            //var logoutUrl
+            return {
+                keycloak: keycloak,
+                loggedIn: loggedIn,
+                logoutUrl: this.logoutUrl
+            };
+        };
+    });
+
+    app.factory('authInterceptor', function($q, keycloakLauncher) {
+        return {
+            request: function (config) {
+                var deferred = $q.defer();
+                var keycloak = keycloakLauncher.keycloak;
+                console.log("authint", keycloakLauncher.loggedIn);
+                if (keycloak.token) {
+                    keycloak.updateToken(5).success(function() {
+                        config.headers = config.headers || {};
+                        config.headers.Authorization = 'Bearer ' + keycloak.token;
+
+                        deferred.resolve(config);
+                    }).error(function() {
+                        deferred.reject('Failed to refresh token');
+                    });
+                }else{
+                    return config;
+                }
+                return deferred.promise;
+            }
+        };
+    });
+
+    app.factory('errorInterceptor', function($q) {
+        return function(promise) {
+            return promise.then(function(response) {
+                return response;
+            }, function(response) {
+                if (response.status == 401) {
+                    console.log('session timeout?');
+                    logout();
+                } else if (response.status == 403) {
+                    alert("Forbidden");
+                } else if (response.status == 404) {
+                    alert("Not found");
+                } else if (response.status) {
+                    if (response.data && response.data.errorMessage) {
+                        alert(response.data.errorMessage);
+                    } else {
+                        alert("An unexpected server error has occurred");
+                    }
+                }
+                return $q.reject(response);
+            });
+        };
+    });
+
+    app.config(function($httpProvider, $routeProvider, keycloakLauncherProvider) {
+        keycloakLauncherProvider.keycloak = new Keycloak('keycloak.json');
+        auth.logoutUrl = keycloakAuth.authServerUrl + "/realms/demo/tokens/logout?redirect_uri=/angular-product-app2/index.html";
+        $httpProvider.interceptors.push('errorInterceptor');
+        $httpProvider.interceptors.push('authInterceptor');
+        $routeProvider.when('/', {
+                controller: 'ProductController',
+                templateUrl: 'partials/products.htm',
+                resolve: {
+                    init: ['initService', function(init) {
+                        console.log("initservice xxxx",init.promise);
+                        return init.promise;
+                    }]
+                }
+            });
+    });
+
+
+    app.run(['keycloakLauncher','initService',function(keycloakLauncher,init) {
+        console.log("runner");
+        var user = {};
+        keycloakLauncher.keycloak.init({ onLoad: 'check-sso' }).success(function () {
+            console.log("entro");
+            keycloakLauncher.loggedIn = keycloakLauncher.keycloak.authenticated;
+            console.log("run login", keycloakLauncher.loggedIn);
+            init.setLoggedIn(keycloakLauncher.keycloak.authenticated);
+
+            if(keycloakLauncher.keycloak.authenticated) {
+                user.email = keycloakLauncher.keycloak.idTokenParsed.email;
+            }else{
+                user.email = "Anonymous"
+            }
+            init.setUserInfo(user)
+            init.defer.resolve();
+        }).error(function () {
+            window.location.reload();
+            init.defer.reject('Failed to Init KC');
+        });
+
+    }]);
+
+    app.service('initService', ['$q', function ($q) {
+        var d = $q.defer();
+        var loggedIn = false;
+        var userInfo = {};
+        //userInfo.email = "Anonymous"
+
+        return {
+            defer: d,
+            promise: d.promise,
+            setLoggedIn: function (logged) {
+                loggedIn = logged;
+            },
+            isLoggedIn: function () {
+                return loggedIn;
+            },
+            setUserInfo: function (user) {
+                userInfo = user;
+            },
+            getUserInfo: function () {
+                return userInfo;//.getSomeData();
+            }
+        };
+    }]);
+
+})();
